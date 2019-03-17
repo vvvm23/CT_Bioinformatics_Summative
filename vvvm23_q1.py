5da14168da4959a8c8b3272bf63e57f08589943b
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-03-17 13:16:18 +0000

    Created generation of scoring matrix

--- vvvm23_q1.py
+++ vvvm23_q1.py
@@ -2,6 +2,34 @@ import time
 import sys
 
 # My functions here
+def align(seq_1, seq_2):
+    # Set scores for alignments
+    MATCH_A = 4 # A - A
+    MATCH_C = 3 # C - C
+    MATCH_G = 2 # G - G
+    MATCH_T = 1 # T - T
+    MISMATCH = -3 # X Y
+    GAP = -2 # X - _
+
+    # Generate scoring and trace matrix
+    # Both matrices will be n+1 X m+1 where n is length of seq_1 and m is length of seq_2
+    n = len(seq_1)
+    m = len(seq_2)
+    score = [[None for j in range(m+1)] for i in range(n+1)]
+    trace = [[None for j in range(m+1)] for i in range(n+1)]
+
+    # Define initial state
+    # Could may redefine this using list comprehension
+    score[0][0] = 0
+    trace[0][0] = 'E'
+    for j in range(1, m+1):
+        score[0][j] = j*GAP
+        trace[0][j] = 'L'
+    for i in range(1, n+1):
+        score[i][0] = i*GAP
+        trace[i][0] = 'U'
+
+
 
 # Template functions: 
 def displayAlignment(alignment):
@@ -36,6 +64,8 @@ best_alignment = ['A', 'A']
 # Stop the timer
 stop = time.time()
 time_taken=stop-start
+
+align(['A', 'T', 'T', 'G'],['A', 'T', 'T', 'G'])
 
 print('Time taken: '+str(time_taken))
 print('Best (score '+str(best_score)+'):')
