4ede94db94005cd87eac4299dab931dc423a0728
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-03-17 14:25:57 +0000

    complete score matrix population

--- speed_test.py
+++ speed_test.py
@@ -1,16 +1,5 @@
 import time
-import numpy as np
 
-def test(seq):
-    start_time = time.time()
-    for i in range(100):
-        for j in range(100):
-            seq[i] = seq[i] + seq[j]
-
-    print(time.time() - start_time)
-
-p = [1]*1000
-n = np.array(p)
-
-test(p)
-test(n)
+print(max(-1,
+1,
+2))
