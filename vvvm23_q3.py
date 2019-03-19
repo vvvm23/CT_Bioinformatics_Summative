d4618479fb0925720c6322a89ad4aaf88c0e38ef
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-03-19 10:31:20 +0000

    file input and retrieval of data from file

--- vvvm23_q3.py
+++ vvvm23_q3.py
@@ -4,3 +4,13 @@ import numpy as np
 import time
 import sys
 
+def WPGMA(path):
+    file = open(path, 'r')
+    contents = file.read().splitlines()
+    column_headers = contents[0].split(' ')[1:]
+    matrix = [[int(x) for x in contents[y].split(' ')[1:]] for y in range(1, len(contents))] # Quicker to maybe map int?
+    print(column_headers)
+    print(np.array(matrix))
+    file.close()
+
+WPGMA('matrix1.txt')
