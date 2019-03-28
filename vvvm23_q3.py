e072edc835f6feeee527088adcc0aa6595e10ba3
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-03-28 10:49:44 +0000

    made graph display better

--- vvvm23_q3.py
+++ vvvm23_q3.py
@@ -12,20 +12,21 @@ def WPGMA(path):
     matrix = np.array([[int(x) if not x == '0' else np.nan for x in contents[y].split(' ')[1:]] for y in range(1, len(contents))]) # Quicker to maybe map int?
     tree = nx.DiGraph()
     for n in range(len(column_headers)):
-        tree.add_node(column_headers[n], pos=(2*(n+1), 2))
+        tree.add_node(column_headers[n], pos=(0,0))#, pos=(2*(n+1), 2))
 
+    n = len(column_headers)
     print(tree.nodes())
     print(matrix)
     while matrix.shape[0] > 2:
+        print()
         s_index = np.unravel_index(np.nanargmin(matrix), matrix.shape) # Is there a better way to find index of smallest element?
         s_label = [column_headers[s_index[0]], column_headers[s_index[1]]]
 
-        new_row = np.array([(len(column_headers[s_index[0]])*matrix[s_index[0], j] + len(column_headers[s_index[1]])*matrix[s_index[1], j])\
-                    /(len(column_headers[s_index[0]] + column_headers[s_index[1]]))\
+        new_row = np.array([(matrix[s_index[0], j] + matrix[s_index[1], j])/2\
                     if not j == s_index[0] else np.nan for j in [_ for _ in range(matrix.shape[0]) if not _ == s_index[1]]])
         # Create a new node at min point
         new_node = column_headers[s_index[0]] + column_headers[s_index[1]]
-        tree.add_node(new_node, pos=((tree.node[s_label[0]]['pos'][0] + tree.node[s_label[1]]['pos'][0])/2 + 2, tree.node[s_label[0]]['pos'][1] + 2))
+        tree.add_node(new_node, pos=(0,0))#, pos=((tree.node[s_label[0]]['pos'][0] + tree.node[s_label[1]]['pos'][0])/2 + 2, tree.node[s_label[0]]['pos'][1] + 2))
 
         # Create edge from new node to the nodes coinciding at the min points
         tree.add_edge(new_node, column_headers[s_index[0]])
@@ -42,9 +43,26 @@ def WPGMA(path):
         print(matrix)
     
     new_node = column_headers[0] + column_headers[1]
-    tree.add_node(new_node, pos=((tree.node[column_headers[0]]['pos'][0] + tree.node[column_headers[1]]['pos'][0])/2 + 2, tree.node[column_headers[0]]['pos'][1] + 2))
+    tree.add_node(new_node, pos=(0,0))#, pos=((tree.node[column_headers[0]]['pos'][0] + tree.node[column_headers[1]]['pos'][0])/2 + 2, tree.node[column_headers[0]]['pos'][1] + 2))
     tree.add_edge(new_node, column_headers[0])
     tree.add_edge(new_node, column_headers[1])
+
+    nodes = [_ for _ in tree.nodes()]
+    nodes.reverse()
+    root = nodes[0]
+
+    for node in nodes:
+        if node == root:
+            tree.node[node]['pos'] = (n, n)
+
+        node_pos = tree.node[node]['pos']
+        children = [_ for _ in tree.neighbors(node)]
+
+        factor = len(node)
+
+        if len(children):
+            tree.node[children[0]]['pos'] = (node_pos[0] - 1*factor, node_pos[1] - 1)
+            tree.node[children[1]]['pos'] = (node_pos[0] + 1*factor, node_pos[1] - 1)
 
     nx.draw_networkx(tree, pos=nx.get_node_attributes(tree, 'pos'))
     plt.axis('off')
@@ -53,6 +71,6 @@ def WPGMA(path):
 start_time = time.time()
 WPGMA('matrix1.txt')
 WPGMA('matrix2.txt')
-WPGMA('matrix3.txt')
+WPGMA('matrix.10.txt')
 end_time = time.time()
 print('Time taken {0}'.format(end_time - start_time))
