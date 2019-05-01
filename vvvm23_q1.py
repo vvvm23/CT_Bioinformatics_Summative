9ecd9150b475341768d335785e754f8ec726ebbf
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-05-01 22:11:05 +0100

    commented code, image saving

--- vvvm23_q1.py
+++ vvvm23_q1.py
@@ -1,19 +1,17 @@
+########################################
+# Run in python2 for some nice speedup #
+########################################
 import time
 import sys
 
-# My functions here
 def align(seq_1, seq_2):
     # Set scores for alignments
-    # Could hardcode in values once debugging done
     score_dict = {'A': 4,
                   'C': 3,
                   'G': 2,
                   'T': 1}
 
     # Generate scoring and trace matrix
-    # Both maenerating empty matrix..')
-    #matrices will be n+1 X m+1 where n is length of seq_1 and m is length of seq_2
-    #print('Geq_1)
     i_seq_1 = tuple(score_dict[b] for b in seq_1)
     i_seq_2 = tuple(score_dict[b] for b in seq_2)
     n = len(seq_1)
@@ -22,9 +20,6 @@ def align(seq_1, seq_2):
     trace = [[None for j in range(m+1)] for i in range(n+1)]
 
     # Define initial state
-    # Could may redefine this using list comprehension
-    #print('Setting matrix intial conditions..')
-    # -1 E; 0 U; 1 D; 2 L;
     score[0][0] = 0
     trace[0][0] = -1
     for j in range(1, m+1):
@@ -33,35 +28,23 @@ def align(seq_1, seq_2):
     for i in range(1, n+1):
         score[i][0] = i*-2
         trace[i][0] = 0
+
     # Move down columns and then across rows, filling in score and trace matrix
-    #print('Populating score and trace matrix..')
     for i in range(1, n+1):
-        '''print('{0}%'.format((i-1)*100 / n), end='\r')
-        sys.stdout.flush()'''
         for j in range(1, m+1):
             if not i_seq_1[i-1] == i_seq_2[j-1]:
-                '''score[i][j], trace[i][j] = max((score[i-1][j-1] + score_dict['M'], 'D'),
-                                           (score[i][j-1] + score_dict['-'], 'L'),
-                                           (score[i-1][j] + score_dict['-'], 'U'),
-                                           key=k)'''
                 score[i][j], trace[i][j] = max((score[i-1][j-1] -3, 1),
                                            (score[i][j-1] -2, 2),
                                            (score[i-1][j] -2, 0))
             else:
-                '''score[i][j], trace[i][j] = max((score[i-1][j-1] + score_dict[seq_1[i-1]], 'D'),
-                                           (score[i][j-1] + score_dict['-'], 'L'),
-                                           (score[i-1][j] + score_dict['-'], 'U'),
-                                           key=k)'''
                 score[i][j], trace[i][j] = max((score[i-1][j-1] + i_seq_1[i-1], 1),
                                            (score[i][j-1] -2, 2),
                                            (score[i-1][j] -2, 0))
 
-    # Traverse traceback until end found.
-    #print('Computing alignment via traceback..')       
+    # Traverse traceback until end found.     
     alignment = ['', '']
     i, j = n, m # Current index
     direction = trace[i][j]
-    # Maybe here loop until either i or j is 0. Then immediately append remainder of string and gaps.
     while not direction == -1:
         # Append to front
         if direction == 0:
@@ -80,9 +63,6 @@ def align(seq_1, seq_2):
 
         direction = trace[i][j] # Get next direction
 
-    '''import numpy as np
-    print(np.array(score))
-    print(np.array(trace))'''
 
     alignment = list(map(lambda x: x[::-1], alignment))
     return score[n][m], alignment
--- vvvm23_q3.py
+++ vvvm23_q3.py
@@ -1,3 +1,6 @@
+##################
+# Run in python3 #
+##################
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
@@ -5,36 +8,44 @@ import time
 import sys
 
 def WPGMA(path):
+    # Open file
     file = open(path, 'r')
     contents = file.read().splitlines()
     file.close()
     column_headers = contents[0].split(' ')[1:]
+
+    # Generate starting matrix
     matrix = np.array([[int(x) if not x == '0' else np.nan for x in contents[y].split(' ')[1:]] for y in range(1, len(contents))]) # Quicker to maybe map int?
+    
+    # Generate starting graph
     tree = nx.DiGraph()
     for n in range(len(column_headers)):
-        tree.add_node(column_headers[n], pos=(0,0))#, pos=(2*(n+1), 2))
+        tree.add_node(column_headers[n], pos=(0,0))
 
     n = len(column_headers)
     print(tree.nodes())
     print(matrix)
     while matrix.shape[0] > 2:
         print()
-        s_index = np.unravel_index(np.nanargmin(matrix), matrix.shape) # Is there a better way to find index of smallest element?
-        s_label = [column_headers[s_index[0]], column_headers[s_index[1]]]
+        s_index = np.unravel_index(np.nanargmin(matrix), matrix.shape) # Find smallest index
+        s_label = [column_headers[s_index[0]], column_headers[s_index[1]]] # Find label of smallest index
 
+        # Calculate the new row values
         new_row = np.array([(matrix[s_index[0], j] + matrix[s_index[1], j])/2\
                     if not j == s_index[0] else np.nan for j in [_ for _ in range(matrix.shape[0]) if not _ == s_index[1]]])
         # Create a new node at min point
         new_node = column_headers[s_index[0]] + column_headers[s_index[1]]
-        tree.add_node(new_node, pos=(0,0))#, pos=((tree.node[s_label[0]]['pos'][0] + tree.node[s_label[1]]['pos'][0])/2 + 2, tree.node[s_label[0]]['pos'][1] + 2))
+        tree.add_node(new_node, pos=(0,0))
 
         # Create edge from new node to the nodes coinciding at the min points
         tree.add_edge(new_node, column_headers[s_index[0]])
         tree.add_edge(new_node, column_headers[s_index[1]])
 
+        # Delete old row and column
         matrix = np.delete(matrix, (s_index[1]), axis=0)
         matrix = np.delete(matrix, (s_index[1]), axis=1)
 
+        # Insert new row
         matrix[s_index[0], :] = new_row
         matrix[:, s_index[0]] = new_row
         column_headers = [_ for _ in column_headers if not _ in s_label and column_headers.index(_) < s_index[0]] + [column_headers[s_index[0]] + column_headers[s_index[1]]] + [_ for _ in column_headers if not _ in s_label and column_headers.index(_) > s_index[0]]
@@ -42,35 +53,44 @@ def WPGMA(path):
         print(column_headers)
         print(matrix)
     
+    # Add last value in
     new_node = column_headers[0] + column_headers[1]
-    tree.add_node(new_node, pos=(0,0))#, pos=((tree.node[column_headers[0]]['pos'][0] + tree.node[column_headers[1]]['pos'][0])/2 + 2, tree.node[column_headers[0]]['pos'][1] + 2))
+    tree.add_node(new_node, pos=(0,0))
     tree.add_edge(new_node, column_headers[0])
     tree.add_edge(new_node, column_headers[1])
 
+    # Construct tree diagram
+    # Make list of nodes starting at root
     nodes = [_ for _ in tree.nodes()]
     nodes.reverse()
     root = nodes[0]
 
+    # Iterate through nodes
     for node in nodes:
         if node == root:
-            tree.node[node]['pos'] = (n, n)
+            tree.node[node]['pos'] = (n, n) # Set root position
 
         node_pos = tree.node[node]['pos']
         children = [_ for _ in tree.neighbors(node)]
 
         factor = len(node)
 
-        if len(children):
+        if len(children): # If not leaf, set position of children
             tree.node[children[0]]['pos'] = (node_pos[0] - 1*factor, node_pos[1] - 1)
             tree.node[children[1]]['pos'] = (node_pos[0] + 1*factor, node_pos[1] - 1)
 
+    # Draw tree and save
     nx.draw_networkx(tree, pos=nx.get_node_attributes(tree, 'pos'))
     plt.axis('off')
-    plt.show()
+    plt.savefig(path+'.png')
+    plt.close()
 
 start_time = time.time()
 WPGMA('matrix1.txt')
+end_time = time.time()
+print('Time taken {0}'.format(end_time - start_time))
+
+start_time = time.time()
 WPGMA('matrix2.txt')
-WPGMA('matrix.10.txt')
 end_time = time.time()
 print('Time taken {0}'.format(end_time - start_time))
